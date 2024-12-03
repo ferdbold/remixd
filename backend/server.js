a596af54e97ba3ebfb18b6eb9a3f494b9b883f4b
Frédéric Bolduc <me@ferdbold.com>
2024-12-02 20:29:30 -0500

    fix edge

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import { get } from '@vercel/edge-config';
+import { createClient } from '@vercel/edge-config';
 import cors from 'cors';
 
 const app = express();
 const port = 5001;
 
+const edge = createClient("https://edge-config.vercel.com/ecfg_ujrysgakrollwa1ihqqlvifvgdpv?token=7baf9476-97f2-447d-a25e-35b770a87ba7");
+
 app.use(cors());
 app.use(express.json());
 
-const stocks = await get('stocks');
+const stocks = await edge.get('stocks');
 
 app.get('/api/stocks', async (req, res) => {
 	try {
