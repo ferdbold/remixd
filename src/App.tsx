3e5d8104d77678e733b96597071b8204f3332c1e
Frédéric Bolduc <me@ferdbold.com>
2024-12-02 21:54:47 -0500

    Fix type issue

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,8 @@ function App() {
   }
 
   const chartOptions = {
-    xaxis: {
-      type: "time",
-      categories: ["18:00", "18:30", "20:00", "20:30", "21:00", "21:30", "22:00"]
-    },
     stroke: {
-      curve: 'smooth'
+      curve: 'smooth' as const
     },
     legend: {
       labels: {
