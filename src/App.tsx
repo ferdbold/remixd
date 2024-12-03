ff02acf8b664734678470460a67c279fc8aed5ba
Frédéric Bolduc <me@ferdbold.com>
2024-12-02 20:50:58 -0500

    Set up apexcharts

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import { useState, useEffect } from 'react'
-import ItemForm from './components/ItemForm'
-import ItemList from './components/ItemList'
+import Chart from "react-apexcharts";
 
 interface Item {
-  _id: string;
   name: string;
+  color: string;
+  data: number[]
 }
 
 function App() {
-  const [items, setItems] = useState<Item[]>([])
+  const [chartSeries, setChartSeries] = useState<Item[]>([]);
 
   useEffect(() => {
-    fetchStocks()
-  }, [])
+    fetchStocks();
+  }, []);
 
   const fetchStocks = async () => {
     try {
       const response = await fetch('http://localhost:5001/api/stocks')
       const data = await response.json()
-      setItems(data)
+      setChartSeries(data)
     } catch (error) {
       console.error('Error fetching items:', error)
     }
   }
 
-  const addItem = async (name: string) => {
+  /*const addItem = async (name: string) => {
     try {
       const response = await fetch('http://localhost:5001/api/items', {
         method: 'POST',
@@ -38,14 +38,50 @@ function App() {
     } catch (error) {
       console.error('Error adding item:', error)
     }
-  }
+  }*/
+
+  const chartOptions = {
+    chart: {
+      id: "basic-bar"
+    },
+    xaxis: {
+      categories: ["18:00", "18:30", "20:00", "20:30", "21:00", "21:30", "22:00"]
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    legend: {
+      labels: {
+        colors: 'grey',
+      },
+      markers: {
+        size: 10,
+        strokeWidth: 0
+      }
+    }
+  };
 
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">Vite + React + MongoDB App</h1>
+    <main className={"flex flex-col min-h-screen bg-slate-900 text-lime-500 px-2"}>
+      <header className={"my-4"}>
+        <h1 className={"text-6xl font-bold text-center"}>R.E.M.I.X.D.</h1>
+        <h2 className={"italic text-sm text-center"}>Real Equity Maximum Income eXchange Department</h2>
+      </header>
+
+      {!!chartSeries && <Chart
+        options={chartOptions}
+        series={chartSeries}
+        type="line"
+        height="75%"
+      />}
+    </main>
+  );
+  /*<div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+    <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+      <div
+        className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+      <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">Vite + React + MongoDB App</h1>
           <div className="max-w-md mx-auto">
             <ItemForm onAddItem={addItem} />
             <ItemList items={items} />
@@ -53,7 +89,7 @@ function App() {
         </div>
       </div>
     </div>
-  );
+  );*/
 }
 
 export default App;
