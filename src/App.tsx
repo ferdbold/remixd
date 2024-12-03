4367e3748a63abab73a80efcc46cb1533c7586ad
Frédéric Bolduc <me@ferdbold.com>
2024-12-02 21:49:35 -0500

    remove starter stuff

--- src/App.tsx
+++ src/App.tsx
@@ -24,22 +24,6 @@ function App() {
     }
   }
 
-  /*const addItem = async (name: string) => {
-    try {
-      const response = await fetch('http://localhost:5001/api/items', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name }),
-      })
-      const newItem = await response.json()
-      setItems([...items, newItem])
-    } catch (error) {
-      console.error('Error adding item:', error)
-    }
-  }*/
-
   const chartOptions = {
     xaxis: {
       type: "time",
@@ -74,20 +58,6 @@ function App() {
       />}
     </main>
   );
-  /*<div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-    <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-      <div
-        className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-      <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-        <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">Vite + React + MongoDB App</h1>
-          <div className="max-w-md mx-auto">
-            <ItemForm onAddItem={addItem} />
-            <ItemList items={items} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );*/
 }
 
 export default App;
