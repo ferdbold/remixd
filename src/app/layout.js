bae32c95076b14b05d1e08117b5a13e79090e1f0
Frédéric Bolduc <me@ferdbold.com>
2024-12-05 22:31:20 -0500

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,10 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <head>
+      <link rel="icon" type="image/jpg" href="favicon.jpg"/>
+    </head>
+    <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
