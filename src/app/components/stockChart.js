29a819a80419a78e2aec2dd2aeba9efd4669a6da
Frédéric Bolduc <me@ferdbold.com>
2024-12-05 20:47:02 -0500

    Stock selector

--- src/app/components/stockChart.js
+++ src/app/components/stockChart.js
@@ -9,6 +9,7 @@ Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Colors);
 
 const StockChart = () => {
   const [chartData, setChartData] = useState([]);
+  const [selectedStock, setSelectedStock] = useState("");
   const containerRef = useRef(null);
 
   useEffect(() => {
@@ -23,12 +24,25 @@ const StockChart = () => {
   if (chartData.length === 0)
     return <></>;
 
-  return <div className="flex-1" ref={containerRef}>
-    <ChartWrapper
-      data={chartData}
-      width={containerRef.current !== null ? containerRef.current.offsetWidth : 100}
-      height={containerRef.current !== null ? containerRef.current.offsetHeight : 100}
-    />
+  return <div className="flex-1 flex flex-col w-full">
+    <div className="w-full flex-1" ref={containerRef}>
+      <ChartWrapper
+        data={chartData}
+        width={containerRef.current !== null ? containerRef.current.offsetWidth : 100}
+        height={containerRef.current !== null ? containerRef.current.offsetHeight : 100}
+      />
+    </div>
+      <ul className="flex flex-row my-2 px-2 gap-2 overflow-x-scroll">
+        {chartData.datasets.map((entry) => {
+          return <li key={entry.label}>
+            <button
+              onClick={() => setSelectedStock(entry.label)}
+              className={`${selectedStock === entry.label ? 'bg-green-500 text-white' : 'bg-green-500/25 text-green-500'} relative z-100 px-3 py-0.5 rounded-md font-bold`}>
+                {entry.label}
+            </button>
+          </li>
+        })}
+      </ul>
   </div>
 }
 export default StockChart;
--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,7 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
